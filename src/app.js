fb2b117cae5c9f75e51148d9ea2fafd5fba8eda4
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2024-06-02 00:53:25 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/user.js";
+import roomRouter from "./routes/room.js";
+import orderRouter from "./routes/order.js";
+
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+import { sendEmail } from "./service/sendMail.js";
+
+dotenv.config();
+// khởi tạo
+const app = express();
+const server = http.createServer(app);
+app.use(express.json());
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api", authRouter);
+app.use("/api", userRouter);
+app.use("/api", roomRouter);
+app.use("/api", orderRouter);
+
+mongoose
+    .connect(process.env.URI)
+    .then(() => {
+        console.log("connect db success!");
+    })
+    .catch((error) => console.log(error));
+
+app.get("/", async (req, res) => {
+    res.json("success");
+});
+
+server.listen(process.env.PORT, (req, res) => {
+    try {
+        console.log(`Server is running on port ${process.env.PORT} `);
+    } catch (error) {
+        console.log(error);
+    }
+});
